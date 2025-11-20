1364d567d4debd4315841d8036587de4cf983e3d
Yueling Lu <ylu@stripe.com>
2025-11-20 16:17:19 -0500

    Add .nojekyll file for GitHub Pages
    - Create public/.nojekyll to disable Jekyll processing
    - Install copy-webpack-plugin to copy static files
    - Configure webpack to copy public folder to dist
    - This fixes potential 404 errors on GitHub Pages
    
    Committed-By-Agent: cursor

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => ({
   entry: './src/main.tsx',
@@ -29,6 +30,11 @@ module.exports = (env, argv) => ({
     new HtmlWebpackPlugin({
       template: './index.html',
       inject: 'body'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.', noErrorOnMissing: true }
+      ]
     })
   ],
   devServer: {
